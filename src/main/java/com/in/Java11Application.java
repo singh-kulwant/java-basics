e8a563247ab1d3bf5e5039aa9e1c98ce45f11734
singh.kulwant@gmx.com <singh.kulwant@gmx.com>
2020-02-15 14:21:01 +0530

    proj changed to maven

---
+++ src/main/java/com/in/Java11Application.java
@@ -0,0 +1,13 @@
+package com.in;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Java11Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Java11Application.class, args);
+	}
+
+}
